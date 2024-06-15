8406a89d04b13af05585529cc9d7b0f06f121a7a
Deepak Singla <thejavascriptways@gmail.com>
2024-06-15 14:59:08 -0500

    Added catalalog with one element

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'bot-catalog',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
+  product: IProduct;
+
+  constructor() {
+    this.product = {
+      id: 2,
+      name: 'Friendly bot',
+      description: 'A friendly robot with two eyes and a smile - a friendly bot',
+      price: 945.0,
+      category: "heads",
+      imgName: "head-friendly.png",
+      discount: 0.2
+    };
+
+  }
 
 }
---
+++ src/app/catalog/product.model.ts
@@ -0,0 +1,9 @@
+export interface IProduct{
+    id: number;
+    description: string;
+    name: string;
+    imgName: string;
+    category: string;
+    price: number;
+    discount: number;
+}
